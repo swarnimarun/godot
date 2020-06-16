073d4d69f09d16732ce7b1c29af6bfb36de164ea
Swarnim Arun <swarnimarun11@gmail.com>
2020-06-16 13:30:38 +0530

    Fix a comment in undo fn
    ..nothing special just changed a word... :P

--- core/undo_redo.cpp
+++ core/undo_redo.cpp
@@ -340,7 +340,7 @@ bool UndoRedo::redo() {
 bool UndoRedo::undo() {
 	ERR_FAIL_COND_V(action_level > 0, false);
 	if (current_action < 0) {
-		return false; //nothing to redo
+		return false; //nothing to undo
 	}
 	_process_operation_list(actions.write[current_action].undo_ops.front());
 	current_action--;
